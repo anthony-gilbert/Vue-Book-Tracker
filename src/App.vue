b36a386344b33a3d249f66f7895cbda1ad57f7d3
Anthony Gilbert <anthony.gilbert93@gmail.com>
2023-09-02 01:53:04 -0500

    Initial Vue-Router Setup

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,32 @@
 <template>
   <div id="app">
-     <!-- <md-app md-waterfall md-mode="fixed"> -->
-      <Header msg="Book Tracker" />
-      <Toread v-bind:toreadcount="2" />
-      <Reading v-bind:readingcount="5" />
-      <Read v-bind:readcount="5" />
-    <!-- </md-app> -->
-    <Footer />
-    
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-// import Vue from 'vue'
-import Header from "./components/Header.vue";
-import Toread from "./components/Toread.vue";
-import Reading from "./components/Reading.vue";
-import Read from "./components/Read.vue";
-import Footer from "./components/Footer.vue";
-
-export default {
-  name: "App",
-  components: {
-    Header,
-    Toread,
-    Reading,
-    Read,
-    Footer,
-  }
-};
-</script>
-
 <style>
-body {
-  margin: 0;
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: cadetblue;
 }
 
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,13 +5,17 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 import 'es6-promise/auto'
+import VueRouter from 'vue-router'
+import router from './router'
 
 Vue.use(Vuex)
 Vue.use(VueMaterial)
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
 
